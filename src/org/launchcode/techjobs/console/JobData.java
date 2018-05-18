1527b422d68a591b7b5779a5b631f4ec80e0e8fc
mrkish <mark@jhns.in>
2018-05-17 19:51:31 -0500

    Added case sensitivity to provided code

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -73,12 +73,13 @@ public class JobData {
         loadData();
 
         ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+        String lowerCaseQuery = value.toLowerCase();
 
         for (HashMap<String, String> row : allJobs) {
 
-            String aValue = row.get(column);
+            String aValue = row.get(column).toLowerCase();
 
-            if (aValue.contains(value)) {
+            if (aValue.contains(lowerCaseQuery)) {
                 jobs.add(row);
             }
         }
